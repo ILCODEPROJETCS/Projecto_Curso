a89db124bd9aba4cf2a7aff49e38fa83f99911d0
Pascoal Nzola Tondo <139359421+pascoalnzola@users.noreply.github.com>
2024-08-16 08:31:25 +0100

    bug 01

--- Banco_dados/cadastrar_config.php
+++ Banco_dados/cadastrar_config.php
@@ -1,48 +1,39 @@
 <?php
- echo "<script> alert('Email já cadastrado22222')</script>";
-    include_once("config.php");
+    include("config.php");
     $exist = 0;
-    if (isset($_POST["nome"]) && isset($_POST["sobrenome"]) && isset($_POST["email"])
-        && isset($_POST["sexo"]) && isset($_POST["senha"]) && isset($_POST["senha_confirm"])
-        && isset($_POST["checar"])){
+    $nome = $_POST["nome"];
+    $sobrenome = $_POST["sobrenome"];
+    $email = $_POST["email"];
+    $sexo = $_POST["sexo"];
+    $senha = $_POST["senha"];
 
-        $nome = $_POST["nome"];
-        $sobrenome = $_POST["sobrenome"];
-        $email = $_POST["email"];
-        $sexo = $_POST["sexo"];
-        $senha = $_POST["senha"];
-
-        if($sexo == "Masculino"){
-            $sexo = "M";
-        }
-        else{
-            $sexo = "F";
-        }
-        $consultar = "SELECT * FROM cadastramento";
-        $verif = $conectar->query($consultar)->fetchAll();
-        echo "<script> alert('Email já cadastrado1111')</script>";
-        foreach($v as $verif){
-            if(!($v["email"] == $email)){
-                $exist = 1;
-                echo "<script> alert('Email já cadastrado3333')</script>";
-            }
-            else{
-                $exist = 0;
-            }
-        }
-        if($exist == 1){
-            echo "<script> alert('Email já cadastrado000000')</script>";
-            $inserir = "INSERT INTO cadastramento VALUES (default, '$nome', '$sobrenome', '$email', '$sexo', '$senha')";
-            echo "<script> alert('Email já cadastrado444444')</script>";
-            $result = $conectar->query($inserir);
-            header("Location: ../depoisCadastramento.php");
-        }
-        else{
-            echo "<script> alert('Email já cadastrado')</script>";
-            header("Location: ../Cadastramento.php");
-        } 
-    }  
-    else{
-        header("Location: ../Cadastramento.php");
+    if($sexo == "Masculino"){
+        $sexo = "M";
     }
+    else{
+        $sexo = "F";
+    }
+    $consultar = "SELECT * FROM cadastramento";
+    $verif = $conectar->query($consultar)->fetchAll();
+    echo "<script> alert('Email já cadastrado1111')</script>";
+    foreach($v as $verif){
+        if(!($v["email"] == $email)){
+            $exist = 1;
+            echo "<script> alert('Email já cadastrado3333')</script>";
+        }
+        else{
+            $exist = 0;
+        }
+    }
+    if($exist == 1){
+        echo "<script> alert('Email já cadastrado000000')</script>";
+        $inserir = "INSERT INTO cadastramento VALUES (default, '$nome', '$sobrenome', '$email', '$sexo', '$senha')";
+        echo "<script> alert('Email já cadastrado444444')</script>";
+        $result = $conectar->query($inserir);
+        header("Location: ./depoisCadastramento.php");
+    }
+    else{
+        echo "<script> alert('Email já cadastrado')</script>";
+        header("Location: ../Cadastramento.php");
+    } 
 ?>
--- Banco_dados/login_config.php
+++ Banco_dados/login_config.php
@@ -1,11 +1,13 @@
 <?php
-    include_once("config.php");
+    include("config.php");
     $v = 0;
     if(isset($_POST["email"]) && isset($_POST["senha"]) && (!empty($_POST["email"])) && (!empty($_POST["senha"]))){
         $email = $_POST["email"];
         $senha = $_POST["senha"];
 
         $consultar = "SELECT * FROM cadastramento";
+        $result = $conectar->query($consultar);
+        $dados = $result->fetchAll();
 
         foreach($res as $dados){
             if($res["email"] == $email && $res["senha"] == $senha){
